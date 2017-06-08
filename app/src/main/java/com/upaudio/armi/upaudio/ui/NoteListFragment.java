af023884962bbe901e506165a84490863fd7bc1d
Armaghan Behlum <armaghan23@gmail.com>
2017-06-08 01:24:29 -0400

    Basic Notes UI

---
+++ app/src/main/java/com/upaudio/armi/upaudio/ui/NoteListFragment.java
@@ -0,0 +1,30 @@
+package com.upaudio.armi.upaudio.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.upaudio.armi.upaudio.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NoteListFragment extends Fragment {
+
+
+    public NoteListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_list, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/upaudio/armi/upaudio/ui/NotesActivity.java
@@ -0,0 +1,15 @@
+package com.upaudio.armi.upaudio.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.upaudio.armi.upaudio.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
